c1d67356cb235f97758a7524e4fd07aa9201304b
Yoshihiro Kameda <kameda.sbng@gmail.com>
2010-06-13 22:16:51 +0900

    refactoring

--- youtube.rb
+++ youtube.rb
@@ -4,33 +4,7 @@ $:.unshift File.dirname(__FILE__) + '/lib'
 require 'rubygems'
 require 'sinatra/base'
 require File.dirname(__FILE__) + '/patch/patch_all'
-require 'content_loader'
-require 'cgi'
-
-Entry = Struct.new(:title, :href, :content, :updated)
-
-class YoutubeLoader
-  def initialize(content_loader=ContentLoader.new)
-    @content_loader = content_loader
-  end
-
-  def load_play_list(account)
-    url = "http://gdata.youtube.com/feeds/api/users/#{CGI.escapeHTML(account)}/playlists"
-    data = @content_loader.load_xml(url)
-    entry_nodes = data.xpath(%{//xmlns:entry})
-    convert_to_entry(entry_nodes)
-  end
-
-  def convert_to_entry(nodes)
-    nodes.map {|n|
-      Entry.new(n.xpath('./xmlns:title')[0].text,
-                n.xpath('./gd:feedLink')[0].attributes['href'].text,
-                n.xpath('./xmlns:content[@type="text"]').text,
-                Time.parse(n.xpath('./xmlns:updated').text)
-      )
-    }
-  end
-end
+require 'youtube_loader'
 
 class MyApp < Sinatra::Base
   helpers do
