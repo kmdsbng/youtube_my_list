54bab88ad10a7e6afad0c956af6d0abefde9e440
Yoshihiro Kameda <kameda.sbng@gmail.com>
2010-06-16 20:15:40 +0900

    refactoring

--- lib/youtube_loader.rb
+++ lib/youtube_loader.rb
@@ -2,7 +2,7 @@ require 'content_loader'
 require 'cgi'
 
 Entry = Struct.new(:title, :href, :content, :updated, :playlist_id)
-PlaylistEntry = Struct.new(:title, :href, :content, :updated)
+VideoEntry = Struct.new(:title, :href, :content, :updated)
 
 class YoutubeLoader
   def initialize(content_loader=ContentLoader.new)
@@ -40,7 +40,7 @@ class YoutubeLoader
     nodes.map {|n|
       content = n.xpath('./xmlns:content[@type="text"]')[0]
       media_content = n.xpath('./media:group/media:content[@yt:format="5"]', doc.root.namespaces)[0]
-      PlaylistEntry.new(n.xpath('./xmlns:title')[0].text,
+      VideoEntry.new(n.xpath('./xmlns:title')[0].text,
                 media_content ? media_content.attributes['url'].text : nil,
                 content ? content : nil,
                 Time.parse(n.xpath('./xmlns:updated')[0].text)
@@ -53,18 +53,6 @@ class YoutubeLoader
     doc = @content_loader.load_xml(url)
     entry_nodes = doc.xpath(%{//xmlns:entry})
     convert_to_playlist_entry(doc, entry_nodes)
-  end
-
-  def convert_to_favorites_entry(doc, nodes)
-    nodes.map {|n|
-      content = n.xpath('./xmlns:content[@type="text"]')[0]
-      media_content = n.xpath('./media:group/media:content[@yt:format="5"]', doc.root.namespaces)[0]
-      PlaylistEntry.new(n.xpath('./xmlns:title')[0].text,
-                media_content ? media_content.attributes['url'].text : nil,
-                content ? content : nil,
-                Time.parse(n.xpath('./xmlns:updated')[0].text)
-      )
-    }
   end
 
 end
